0ba16a1ee9cf71d0f72e35c4986f5c0418fca8fd
Marat-mtr <ruslandhost@gmail.com>
2023-06-29 23:23:58 +0300

    first commit

---
+++ resources/js/components/header.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav>
+        <link rel="stylesheet" href="css/bootstrap5/bootstrap.min.css" type="text/css">
+        <div class="container">
+            <ul class="uk-navbar-nav">
+                <li class="uk-active"><a href="#">Головна</a> </li>
+                <li> <a href="#">Угоди</a> </li>
+                <li> Аккаунт
+                    <ul>
+                        <li><a href="#">Вхід</a> </li>
+                        <li><a href="#">Вихідll</a> </li>
+                    </ul>
+                </li>
+            </ul>
+
+        </div>
+    </nav>
+
+</template>
+
+<script>
+export default {
+
+    name: "header"
+
+}
+</script>
+
+<style scoped>
+
+</style>
+
---
+++ resources/views/index.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title> PHP</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap5/bootstrap.min.css" type="text/css">
+</head>
+<body>
+
+<h1> Vse ok</h1>
+
+
+
+
+
+
+
+
+
+{{--<div id="app">--}}
+
+{{--<mainheader></mainheader>--}}
+{{--<example-component></example-component>--}}
+
+{{--</div>--}}
+<script src="./js/app.js"></script>
+<script src="js/bootstrap5js/bootstrap.min.js"></script>
+
+</body>
+</html>
